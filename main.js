9756f69ddacc7f4c05a6559588222dc949373d75
ugo <stau04@gmail.com>
2021-08-05 19:27:14 +0900

    js-study 프로젝트 생성

---
+++ main.js
@@ -0,0 +1,18 @@
+// let box = document.querySelector('.box');
+// let isClicked = false;
+
+// box.addEventListener('click',()=>{
+//     console.log('click!');
+//     box.classList.add('active');
+//     isClicked = box.classList.contains('active');
+// })
+
+console.log('helloo')
+
+
+
+
+
+
+
+
